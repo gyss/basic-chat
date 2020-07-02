401254600c95a06412ac7b77ec1483cd3e1ed2f7
gyss <gravedian@gmail.com>
2020-07-02 22:45:11 +0200

    Added key to Select option list

--- webapp/src/controls/Select.tsx
+++ webapp/src/controls/Select.tsx
@@ -15,15 +15,11 @@ interface IProps {
   options: Array<Option>
 }
 
-export default function Select({
-  options,
-  value,
-  ...props
-}: IProps & React.InputHTMLAttributes<HTMLSelectElement>) {
+export default function Select({options, value, ...props}: IProps & React.InputHTMLAttributes<HTMLSelectElement>) {
   return (
     <select css={selectStyles} {...props}>
       {options.map((option: Option) => (
-        <option value={option.value} selected={value === option.value}>
+        <option value={option.value} key={option.value} selected={value === option.value}>
           {option.label}
         </option>
       ))}
