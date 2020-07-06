c00a54193818b7975673599a08ee9514afef24ab
gyss <gravedian@gmail.com>
2020-07-07 00:14:50 +0100

    updated ChatView tests

--- webapp/src/components/__test__/ChatView/index.test.tsx
+++ webapp/src/components/__test__/ChatView/index.test.tsx
@@ -10,18 +10,7 @@ import {initialState} from '../../../store/reducer'
 import {SET_CHAT_MESSAGE} from '../../../store/actionTypes'
 
 describe('<Message>', () => {
-  test('should render an empty messages list', () => {
-    render(
-      <Context.Provider value={{dispatch: jest.fn(), globalState: initialState}}>
-        <ChatView />
-      </Context.Provider>,
-    )
-
-    const messageMeta = screen.queryByText('No messages yet')
-    expect(messageMeta).toBeInTheDocument()
-  })
-
-  test('should call dispatch with the new message', () => {
+  test('should call dispatch when user clicks on send button', () => {
     const dispatch = jest.fn()
     render(
       <Context.Provider value={{dispatch, globalState: initialState}}>
@@ -31,12 +20,18 @@ describe('<Message>', () => {
 
     const input = screen.getByPlaceholderText('Enter message')
     fireEvent.change(input, {target: {value: 'Hello world'}})
+    expect(dispatch).toHaveBeenLastCalledWith({type: SET_CHAT_MESSAGE, payload: 'Hello world'})
+  })
+
+  test("should not call dispatch when user clicks on send button and there's no message", () => {
+    const dispatch = jest.fn()
+    render(
+      <Context.Provider value={{dispatch, globalState: initialState}}>
+        <ChatView />
+      </Context.Provider>,
+    )
 
     fireEvent.click(screen.getByTestId('send-button'))
-    expect(dispatch).toHaveBeenCalledTimes(1)
-    expect(dispatch).toHaveBeenCalledWith({type: SET_CHAT_MESSAGE, payload: 'Hello world'})
-
-    const messageMeta = screen.queryByText('No messages yet')
-    expect(messageMeta).toBeInTheDocument()
+    expect(dispatch).toHaveBeenCalledTimes(0)
   })
 })
